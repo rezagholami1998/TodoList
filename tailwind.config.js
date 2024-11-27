3ec825370c34634fe59c0410563c0d93960ab8df
reza gholami <reza.gholami.m.1998@gmail.com>
2024-11-27 13:48:38 +0330

    Chore : changeconfig tailwind for responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,25 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      xs: "330px",
+      // => @media (min-width: 330px) { ... }
+
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
   },
   plugins: [],
 };
