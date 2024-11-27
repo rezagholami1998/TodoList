4d434610a99f8876b72b0c6e17c32437bd02bbfe
reza gholami <reza.gholami.m.1998@gmail.com>
2024-11-27 13:49:29 +0330

    Add : route Home ,Verification and Notfound to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Notfound from "./pages/Notfound";
 const App = () => {
-  return <>Hi!</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<Notfound />} />
+        <Route path="/verification" element={<></>} />
+        <Route path="/home" element={<></>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
-
 export default App;
