c9c12cbc9f6e29b557501a75a5a3e03371cde1c9
reza gholami <reza.gholami.m.1998@gmail.com>
2024-11-28 11:33:58 +0330

    Style : change style component NotFound

--- src/pages/Notfound.tsx
+++ src/pages/Notfound.tsx
@@ -1,14 +1,15 @@
 import NotFoundImg from "../assets/images/notFound.svg";
+///////////////////////////////////////////////////////
 const Notfound = () => {
   return (
     <div className="w-full h-full flex flex-col items-center justify-center">
-      <img src={NotFoundImg} className="xs:max-w-[100%] xs:px-5 sm:w-auto" />
-      <h5 className="mt-6 color-[#2E3947] font-bold leading-6 xs:text-[1.25rem] sm:text-[1.5rem]">
-        !صفحه مورد نظر یافت نشد
-      </h5>
-      <p className="text-[.876rem] leading-6 mt-4">
-        .آدرس وارد شده را بررسی نمایید
-      </p>
+      <div className="text-center xs:mx-4 sm:mx-0">
+        <img src={NotFoundImg} className="xs:max-w-[100%]" />
+        <h5 className="font-bold mt-6 leading-6 xs:text-lg sm:text-xl">
+          صفحه مورد نظر یافت نشد
+        </h5>
+        <p className="text-sm leading-6 mt-2">آدرس وارد شده را بررسی نمایید</p>
+      </div>
     </div>
   );
 };
